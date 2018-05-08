2bf2573285de5c0a9b98afd70f3ebea2b65640bf
Javier Lozano Gadín <javier.lozano@udc.es>
2018-05-08 17:00:12 +0200

    Creación arquetipo Maven y documentación de la aplicación iTarea.

---
+++ src/main/java/es/jvcorp/iTarea/App.java
@@ -0,0 +1,13 @@
+package es.jvcorp.iTarea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/sql/1-MySQLCreateTables.sql
@@ -0,0 +1,12 @@
+-- ----------------------------------------------------------------------------
+-- "iTarea" Create Tables.
+-------------------------------------------------------------------------------
+
+-- Indexes for primary keys have been explicitly created.
+
+-- ---------- Table for validation queries from the connection pool -----------
+
+DROP TABLE IF EXISTS PingTable;
+CREATE TABLE PingTable (foo CHAR(1));
+
+
